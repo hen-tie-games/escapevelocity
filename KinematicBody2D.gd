2eb848fc9aecb10cce91355b9a94f9ac11e1f7f9
gucker <nman4gr@yahoo.com>
2019-09-14 03:14:39 -0400

    adding untracked files

---
+++ KinematicBody2D.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+const MAX_SPEED = 150
+const ACCELERATION = 25
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x =min(motion.x+ACCELERATION,MAX_SPEED)
+		
+	elif Input.is_action_pressed("ui_left"):
+		motion.x =max(motion.x-ACCELERATION,-MAX_SPEED)
+		
+	elif Input.is_action_pressed("ui_up"):
+		motion.y =max(motion.y-ACCELERATION,-MAX_SPEED)
+		
+	elif Input.is_action_pressed("ui_down"):
+		motion.y =min(motion.y+ACCELERATION,MAX_SPEED)
+		
+	
+	motion = move_and_slide(motion)
